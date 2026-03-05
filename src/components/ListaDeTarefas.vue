32cf303196cbf3c199421caca8a0640798f3b7ef
DanielSouza-de <daniel.desouza2007@gmail.com>
2026-03-05 13:31:43 -0300

    componentização

--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -1,11 +1,30 @@
+<script setup>
+const props = defineProps(['tarefas']);
+</script>
+
 <template>
-    <ul class="list-group mt-4">
-        <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
-            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
-                :id="tarefa.titulo" type="checkbox">
-            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
-                {{ tarefa.titulo }}
-            </label>
-        </li>
-    </ul>
+    <div class="mt-4">
+
+        <ul v-if="props.tarefas.length" class="list-group">
+            <li class="list-group-item" v-for="tarefa in props.tarefas" :key="tarefa.titulo">
+                <input type="checkbox" :checked="tarefa.finalizada"
+                    @change="evento => tarefa.finalizada = evento.target.checked" :id="tarefa.titulo">
+
+                <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                    {{ tarefa.titulo }}
+                </label>
+            </li>
+        </ul>
+
+        <p v-else class="text-center mt-4">
+            Nenhuma tarefa encontrada
+        </p>
+
+    </div>
 </template>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
